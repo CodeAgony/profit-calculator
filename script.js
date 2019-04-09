9f871ef783b39c90e3d4ab93c6794aa4a89ade77
CodeAgony <yaroslav.bkh@gmail.com>
2019-04-09 14:32:36 +0300

    Remove reset function

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
-    const calcProfit = function(){    
-
+// document.addEventListener('change', calcProfit)
         let buyPrice = document.getElementById("buyPrice").value;
         let amount = document.getElementById("amount").value;
         let sellPrice = document.getElementById("sellPrice").value;
         let tax = document.getElementById("tax").value;
+        let profitSilverField = document.getElementById("profitSilver").value;
+        let profitMarginField = document.getElementById("profitMargin").value;
 
-        const profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer `;
+    const calcProfit = function(){    
+
+        const profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer`;
         console.log(profitSilver);
-        document.getElementById("profitSilver").value = profitSilver;
+        
 
 
-        const profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}% `;
+        const profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}%`;
         console.log(profitMargin);
-        document.getElementById("profitMargin").value = profitMargin;
     }
 
-    const resetOnClick = function(){
-
-        document.getElementById("tax").value =
-        document.getElementById("sellPrice").value =
-        document.getElementById("amount").value =
-        document.getElementById("buyPrice").value = 
-        document.getElementById("profitSilver").value =
-        document.getElementById("profitMargin").value = "";
-        console.log("reset clicked");
-    } 
-
+    document.getElementById('button-calc').addEventListener('click', calcProfit);
+    document.getElementById('button-reset').addEventListener('click', reset); 
 })
