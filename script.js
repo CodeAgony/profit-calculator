4adaa806a8efd38faf77bbd8608b7f2a7ad357df
CodeAgony <yaroslav.bkh@gmail.com>
2019-04-09 09:26:40 +0300

    Fix variable and constant declaration

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-  var calcProfit = function(){    
+    const calcProfit = function(){    
 
-    var buyPrice = document.getElementById("buyPrice").value;
-    var amount = document.getElementById("amount").value;
-    var sellPrice = document.getElementById("sellPrice").value;
-    var tax = document.getElementById("tax").value;
+        let buyPrice = document.getElementById("buyPrice").value;
+        let amount = document.getElementById("amount").value;
+        let sellPrice = document.getElementById("sellPrice").value;
+        let tax = document.getElementById("tax").value;
 
-    var profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer `;
-    console.log(profitSilver);
-    document.getElementById("profitSilver").value = profitSilver;
+        const profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer `;
+        console.log(profitSilver);
+        document.getElementById("profitSilver").value = profitSilver;
 
 
-    var profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}% `;
-    console.log(profitMargin);
-    document.getElementById("profitMargin").value = profitMargin;
+        const profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}% `;
+        console.log(profitMargin);
+        document.getElementById("profitMargin").value = profitMargin;
 
   }
 
-  var resetOnClick = function(){
+  const resetOnClick = function(){
     document.getElementById("tax").value =
     document.getElementById("sellPrice").value =
     document.getElementById("amount").value =
