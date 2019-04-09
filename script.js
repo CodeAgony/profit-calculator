53cd32e31534789a25fd23fa23ae5dae8f2ad9d8
CodeAgony <yaroslav.bkh@gmail.com>
2019-04-09 14:40:31 +0300

    Remove event listener

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
 // document.addEventListener('change', calcProfit)
+
+    const calcProfit = function(){    
         let buyPrice = document.getElementById("buyPrice").value;
         let amount = document.getElementById("amount").value;
         let sellPrice = document.getElementById("sellPrice").value;
         let tax = document.getElementById("tax").value;
-        let profitSilverField = document.getElementById("profitSilver").value;
-        let profitMarginField = document.getElementById("profitMargin").value;
+        let profitSilver = document.getElementById("profitSilver").value;
+        let profitMargin = document.getElementById("profitMargin").value;
 
-    const calcProfit = function(){    
-
-        const profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer`;
+        profitSilver = `You are ${Math.round((sellPrice*amount-(sellPrice*amount/100)*tax)-buyPrice*amount)} silver richer`;
         console.log(profitSilver);
         
 
 
-        const profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}%`;
+        profitMargin = `Profit margin is ${Math.round(document.getElementById("profitSilver").value/(buyPrice*amount)*100)}%`;
         console.log(profitMargin);
     }
 
     document.getElementById('button-calc').addEventListener('click', calcProfit);
-    document.getElementById('button-reset').addEventListener('click', reset); 
 })
